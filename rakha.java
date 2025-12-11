333679d114d8f7d1552509ec62ad3fe16f0c16ad
Muhammad Rakha Fadhlany <rakhafadhlany@gmail.comgit config --list;/;;git config --global user.email rakhafadhlany@gmail.com>
2025-12-11 11:12:52 +0700

    first commit Rakha

--- rakha.java
+++ rakha.java
@@ -1,5 +1,5 @@
 public class rakha {
     public static void main(String[] args) {
-        System.out.println("saya rakha- 1324045!");
+        System.out.println("saya Muhaammad Rakha Fadhlany - 1324045!");
     }
 }
